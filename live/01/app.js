1b8f93da3b27da47972beeabaa51b443ec44496a
ArniWebDev <arnoldhorvath@192-168-0-134.rdsnet.ro>
2023-06-13 22:09:47 +0300

    forEach and for...of live

--- live/01/app.js
+++ live/01/app.js
@@ -41,9 +41,43 @@ const szamol = (elem) => {
 	szamlaTomb.push({ ...elem, ertek });
 };
 
-for (let i = 0; i < tomb.length; i++) {
-	szamol(tomb[i]);
-}
+// for (let i = 0; i < tomb.length; i++) {
+// 	szamol(tomb[i]);
+// }
 
+// for (const elem of tomb) {
+// 	console.log(elem);
+// }
+
+// const thisEntries = tomb.values();
+// console.log(thisEntries.next());
+
+// let elem = thisEntries.next();
+// console.log(elem)
+
+// while (!elem.done) {
+// 	console.log(elem.value);
+// 	szamol(elem.value);
+// 	elem = thisEntries.next();
+// }
 console.log(szamlaTomb);
 console.log({ osszeg });
+
+// try {
+// tomb.forEach((element, index, array) => {
+// 	console.log(element, index, array);
+// 	if (element.ar > 100) {
+// 		console.log("drága");
+// 		throw new Error("drága");
+// 	}
+// });
+// } catch (error) {
+// 	console.error(error.message);
+// }
+
+tomb.forEach(({ar, db}) => {
+	console.log(ar * db);
+	if (ar * db > 400) {
+		console.log("drága");
+	}
+});
